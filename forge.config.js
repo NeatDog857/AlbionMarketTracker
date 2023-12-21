06337e85673f525800fd56fe7a8462786bf7f432
NeatDog857 <doublecheng06@gmail.com>
2023-12-21 22:51:30 +0800

    20231221 publish test

--- forge.config.js
+++ forge.config.js
@@ -3,6 +3,19 @@ module.exports = {
     asar: true,
   },
   rebuildConfig: {},
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'NeatDog857',
+          name: 'AlbionMarketTracker'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
